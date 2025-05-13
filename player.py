5dd7f6854555777dc0f575a0026dfca34c6cc27f
N N <126a23@gmail.com>
2025-05-13 21:35:47 +0300

    Add files via upload
    Скрипт игрока обновлён

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, platforms):
         super().__init__()
         self.image = pygame.Surface((30, 50))
         self.image.fill(G)
@@ -14,22 +14,61 @@ class Player(pygame.sprite.Sprite):
         self.health = PLAYER_HEALTH
         self.score = 0
         self.is_jumping = False
-
-
-    def update(self):
+        self.velocity_y = 0
+        self.platforms = platforms  # Теперь игрок знает о платформах
+        
+    def update(self, enemies):
         keys = pygame.key.get_pressed()
 
-        #Движение
+        # Горизонтальное движение
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
         if keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
-        
+
         # Прыжок
         if keys[pygame.K_SPACE] and not self.is_jumping:
             self.is_jumping = True
-            self.rect.y -= self.jump_power
-        
+            self.velocity_y = -self.jump_power
+
+        # # Гравитация
+        # self.velocity_y += 0.8
+        # self.rect.y += self.velocity_y
+
+        # # Проверка приземления на платформы
+        # self.is_jumping = True
+        # for platform in self.platforms:
+        #     if (self.rect.colliderect(platform.rect) and
+        #         self.velocity_y >= 0 and
+        #         self.rect.bottom <= platform.rect.top + 5):
+        #         self.rect.bottom = platform.rect.top
+        #         self.is_jumping = False
+        #         self.velocity_y = 0
+        #         break
+# Гравитация
+        self.velocity_y += 0.8
+        dy = self.velocity_y
+
+        # Проверка столкновений по вертикали
+        self.rect.y += dy
+        self.is_jumping = True
+
+        for platform in self.platforms:
+            if self.rect.colliderect(platform.rect):
+                if dy > 0 and self.rect.bottom <= platform.rect.top + 10:
+                    self.rect.bottom = platform.rect.top
+                    self.velocity_y = 0
+                    self.is_jumping = False
+        # Проверка прыжка на врагов
+        for enemy in enemies:
+            if (self.rect.colliderect(enemy.rect) and
+                self.velocity_y >= 0 and
+                self.rect.bottom <= enemy.rect.top + 5):
+                enemy.kill()
+                self.score += 10
+                self.velocity_y = -5  # Отскок
+
+                
     def take_damage(self, damage):
         self.health -= damage
         if self.health <= 0:
