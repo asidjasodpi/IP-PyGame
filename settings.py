ab669aff9b93fddb8aa8898f99c9f839b10d8624
N N <126a23@gmail.com>
2025-05-13 20:42:14 +0300

    Add files via upload
    Добавлен скрипт со всеми используемыми константами для кода

---
+++ settings.py
@@ -0,0 +1,21 @@
+#Окно
+WIDTH = 1280
+HEIGHT = 720
+FPS = 60
+
+#Игрок
+PLAYER_SPEED = 5
+GRAVITY = 0.5
+PLAYER_HEALTH = 100
+PLAYER_JUMP_POWER = 250
+
+#Цвета (Пока нету спрайтов)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+R = (255, 0, 0)
+G = (0, 255, 0)
+B = (0, 0, 255)
+
+#Враги
+ENEMY_SPEED = 3
+WAVE_DELAY = 10000
