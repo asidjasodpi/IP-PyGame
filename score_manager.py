b16d5d46731f5b8d32bf1403e569f1bb312453a2
N N <126a23@gmail.com>
2025-05-21 19:31:12 +0300

    Add files via upload
    Скрипт обновлён

--- score_manager.py
+++ score_manager.py
@@ -1,10 +1,11 @@
 def load_score():
     try:
-        with open("score.txt","r") as file:
+        with open("score.txt", "r") as file:
             return int(file.read())
     except (FileNotFoundError, ValueError):
+        # Если файла нет или он пустой, вернём 0
         return 0
-    
+
 def save_score(score):
     with open("score.txt", "w") as file:
         file.write(str(score))
