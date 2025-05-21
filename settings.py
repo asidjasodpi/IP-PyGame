b8bf23f9e9ace2f2ba55bdbf2401772249b5956b
N N <126a23@gmail.com>
2025-05-21 19:31:34 +0300

    Add files via upload
    Скрипт обновлён

--- settings.py
+++ settings.py
@@ -1,21 +1,18 @@
-#Окно
-WIDTH = 1280
-HEIGHT = 720
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 FPS = 60
 
-#Игрок
-PLAYER_SPEED = 5
-GRAVITY = 0.5
-PLAYER_HEALTH = 100
-PLAYER_JUMP_POWER = 250
-
-#Цвета (Пока нету спрайтов)
+# Цвета
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-R = (255, 0, 0)
-G = (0, 255, 0)
-B = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
 
-#Враги
-ENEMY_SPEED = 3
-WAVE_DELAY = 10000
+# Игрок
+PLAYER_SPEED = 10
+PLAYER_JUMP_POWER = 10
+PLAYER_HEALTH = 1000
+# Враги
+ENEMY_SPEED = 2
+WAVE_DELAY = 5000  # 5 сек между волнами
